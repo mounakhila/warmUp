03c9f242cd550c0aa88361edb281184aabda01c4
mounakhila <khilamouna@yahoo.fr>
2020-03-13 12:48:44 +0100

    help

--- warmUp1.js
+++ warmUp1.js
@@ -4,3 +4,34 @@
 // 4-calculate your age in seconds.
 
 // your code is here
+1)
+'firstname' + ' ' +'lastname'
+
+
+2)
+13%3=1 : 13 is not a multiple of 3
+
+3)
+function averageages(array){
+	var sum=0;
+	for (i = 0; i < array.length; i++){
+		avr=sum/array.length[i];
+	}
+    }
+    return result }
+    averageages
+
+
+
+
+
+
+
+	}
+}
+// 1-using + operator combine your partner first and last name .
+// 2-find if the number 13 is a multiple of 3 or not.
+// 3-calculate the average age of the follwing ages [13,14,13,15,16,17,19,13,16,15]
+// 4-calculate your age in seconds.
+
+// your code is here
