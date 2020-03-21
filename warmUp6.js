db0f263c33526c9813e110918d0b2c52c7bc70e8
mounakhila <khilamouna@yahoo.com>
2020-03-21 18:52:33 +0100

    thank you letting me practicing

--- warmUp6.js
+++ warmUp6.js
@@ -8,3 +8,4 @@
 // 3-Write a JavaScript function that accepts a number as a parameter and check if the number is prime or not.
 
 // 4-write a function that accepts two numbers as parameters and returns true if the two parameters have the same value , false if not
+my test thank you letting me practicing 
